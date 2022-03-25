b1e802477c63f62b555c71872b95b31978a770b6
Cute_Wisp <sweatpotato13@gmail.com>
2022-03-25 22:18:11 +0900

    fix: 🐛 remove transaction in scheduler

--- src/modules/scheduler/app/scheduler.service.ts
+++ src/modules/scheduler/app/scheduler.service.ts
@@ -16,36 +16,28 @@ import {
 export class SchedulerService {
     constructor(private readonly _commandBus: CommandBus) {}
 
-    @Transactional()
     public async parseTransactionData(): Promise<any> {
         try {
             const ret = await this._commandBus.execute(
                 new ParseTransactionDataCommand()
             );
-            runOnTransactionCommit(() => {});
             return ret;
         } catch (error) {
-            runOnTransactionRollback(() => {});
             throw error;
         } finally {
-            runOnTransactionComplete(() => {});
         }
     }
 
-    @Transactional()
     @Cron(CronExpression.EVERY_DAY_AT_2AM)
     public async parseUniswapData(): Promise<any> {
         try {
             const ret = await this._commandBus.execute(
                 new ParseUniswapDataCommand()
             );
-            runOnTransactionCommit(() => {});
             return ret;
         } catch (error) {
-            runOnTransactionRollback(() => {});
             throw error;
         } finally {
-            runOnTransactionComplete(() => {});
         }
     }
 
@@ -55,13 +47,10 @@ export class SchedulerService {
             const ret = await this._commandBus.execute(
                 new ParseTransactionDataCommand()
             );
-            runOnTransactionCommit(() => {});
             return ret;
         } catch (error) {
-            runOnTransactionRollback(() => {});
             throw error;
         } finally {
-            runOnTransactionComplete(() => {});
         }
     }
 
